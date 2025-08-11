b394077baebfed771ba45d7411f4aa8c6386816b
Lucas Yuzo Kihana <lucas.kihana@gmail.com>
2025-08-10 21:38:58 -0300

    fix `/migrations` endpoint connection bug

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,6 @@
 import database from "infra/database";
 
-export default async function statusController(request, response) {
+export default async function status(request, response) {
   const updatedAt = new Date().toISOString();
 
   const databaseVersionResult = await database.query("SHOW server_version;");
